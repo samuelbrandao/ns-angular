5e88bd6cbff574f7e02b79cbf5a1be3523a18377
Samuel <samuelbrandao@gmail.com>
2019-04-01 16:13:27 -0300

    s5c96

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -11,7 +11,7 @@ export class CurrentChallengeComponent {
   constructor(private router: RouterExtensions) {}
 
   onEdit() {
-    this.router.navigate(['/edit-challenge'], {
+    this.router.navigate(['/challenges/edit'], {
       transition: { name: 'slideLeft' }
     });
   }
