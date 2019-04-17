f1e2c794dbb62b5ea73632c8def4627d1dc33312
Samuel <samuelbrandao@gmail.com>
2019-04-17 11:48:51 -0300

    139

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -22,6 +23,7 @@ import { ChallengeActionsModule } from './challenges/challenge-actions/challenge
     NativeScriptModule,
     NativeScriptFormsModule,
     NativeScriptUISideDrawerModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     SharedModule,
     ChallengeActionsModule
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { TextField } from 'tns-core-modules/ui/text-field';
 
 @Component({
   selector: 'ns-auth',
@@ -8,11 +10,38 @@ import { RouterExtensions } from 'nativescript-angular/router';
   moduleId: module.id
 })
 export class AuthComponent implements OnInit {
+  form: FormGroup;
+  emailControlIsValid = true;
+  passwordControlIsValid = true;
+  @ViewChild('passwordEl') passwordEl: ElementRef<TextField>;
+  @ViewChild('emailEl') emailEl: ElementRef<TextField>;
+
   constructor(private router: RouterExtensions) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      })
+    });
+  }
 
   onSignin() {
     this.router.navigate(['/today'], { clearHistory: true });
+  }
+
+  onSubmit() {
+    this.emailEl.nativeElement.focus();
+    this.passwordEl.nativeElement.focus();
+    this.passwordEl.nativeElement.dismissSoftInput();
+
+    const email = this.form.get('email').value;
+    const password = this.form.get('password').value;
+    console.log(email, password);
   }
 }
